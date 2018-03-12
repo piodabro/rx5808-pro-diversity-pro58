bf281c57c206ccbed5bc726f7d5a0c0e83aa4221
Mikko Rosten <mikko.rosten@iki.fi>
2018-03-12 23:54:04 +0200

    Fix compilation on buttons in hb5808
    no fs pins on hb5808
    
    Signed-off-by: Mikko Rosten <mikko.rosten@iki.fi>

--- PRO58/Src/buttons.cpp
+++ PRO58/Src/buttons.cpp
@@ -26,42 +26,32 @@ namespace Buttons {
 
 
     void update() {
-/*        #define UPDATE_BUTTON(button) \
-            updateButton( \
-                Button::button, \
-                states[static_cast<__SIZE_TYPE__>(Button::button)], \
-                BUTTON_ ## button ## _GPIO_Port \
-				BUTTON_ ## button ## _Pin \
-            );
-
-        UPDATE_BUTTON(UP);
-        UPDATE_BUTTON(DOWN);
-        UPDATE_BUTTON(MODE);
-        UPDATE_BUTTON(EB0);
-        UPDATE_BUTTON(EB1);
-        UPDATE_BUTTON(EB2);
-        #undef UPDATE_BUTTON
-*/
     	if(!EepromSettings.screenFlip){
 			updateButton(Button::UP, states[static_cast<uint8_t>(Button::UP)], BUTTON_UP_GPIO_Port, BUTTON_UP_Pin);
 			updateButton(Button::DOWN, states[static_cast<uint8_t>(Button::DOWN)], BUTTON_DOWN_GPIO_Port, BUTTON_DOWN_Pin);
+#ifndef HB5808
 			if(!EepromSettings.FSPinsMode){
 				updateButton(Button::UP, states[static_cast<uint8_t>(Button::FS_UP)], FS_PIN_EB0_GPIO_Port, FS_PIN_EB0_Pin);
 				updateButton(Button::DOWN, states[static_cast<uint8_t>(Button::FS_DOWN)], FS_PIN_EB2_GPIO_Port, FS_PIN_EB2_Pin);
 			}
+#endif
     	}
     	else{
 			updateButton(Button::UP, states[static_cast<uint8_t>(Button::UP)], BUTTON_DOWN_GPIO_Port, BUTTON_DOWN_Pin);
 			updateButton(Button::DOWN, states[static_cast<uint8_t>(Button::DOWN)], BUTTON_UP_GPIO_Port, BUTTON_UP_Pin);
+#ifndef HB5808
 			if(!EepromSettings.FSPinsMode){
 				updateButton(Button::UP, states[static_cast<uint8_t>(Button::FS_UP)], FS_PIN_EB2_GPIO_Port, FS_PIN_EB2_Pin);
 				updateButton(Button::DOWN, states[static_cast<uint8_t>(Button::FS_DOWN)], FS_PIN_EB0_GPIO_Port, FS_PIN_EB0_Pin);
 			}
+#endif
     	}
 		updateButton(Button::MODE, states[static_cast<uint8_t>(Button::MODE)], BUTTON_MODE_GPIO_Port, BUTTON_MODE_Pin);
+#ifndef HB5808
 		if(!EepromSettings.FSPinsMode){
 			updateButton(Button::MODE, states[static_cast<uint8_t>(Button::FS_MODE)], FS_PIN_EB1_GPIO_Port, FS_PIN_EB1_Pin);
 		}
+#endif
     }
 
     const ButtonState *get(Button button) {
